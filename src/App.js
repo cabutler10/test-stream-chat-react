d9d6fb2019eb7b6dd9a98d3c3337477c38f205c0
Alyssa <ca.butler10@gmail.com>
2021-04-08 09:46:56 +0200

    add demo

--- src/App.js
+++ src/App.js
@@ -1,45 +1,58 @@
 import React from "react";
 import { StreamChat } from "stream-chat";
 import {
-  Chat,
   Channel,
   ChannelHeader,
+  ChannelList,
+  ChannelListMessenger,
+  ChannelPreviewMessenger,
+  Chat,
   MessageInput,
+  MessageInputFlat,
   MessageList,
   Thread,
   Window,
 } from "stream-chat-react";
-
 import "stream-chat-react/dist/css/index.css";
 
-const chatClient = StreamChat.getInstance("z6cfpdzya8qb");
+const apiKey = "z6cfpdzya8qb";
+const userId = "fancy-feather-7";
 const userToken =
   "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiZmFuY3ktZmVhdGhlci03In0.GbmsXHJJQu19xEo5CzFL-euKvzFEGow4RrtqGo9vqMs";
+const theme = "light";
 
-chatClient.connectUser(
-  {
-    id: "fancy-feather-7",
-    name: "fancy-feather-7",
-    image:
-      "https://getstream.io/random_png/?id=fancy-feather-7&name=fancy-feather-7",
-  },
-  userToken
-);
+const filters = { type: "messaging" };
+const options = { state: true, presence: true, limit: 10 };
+const sort = {
+  cid: 1,
+  last_message_at: -1,
+  updated_at: -1,
+};
 
-const channel = chatClient.channel("messaging", "fancy-feather-7", {
-  // add as many custom fields as you'd like
-  image: "https://www.drupal.org/files/project-images/react.png",
-  name: "Talk about React",
-  members: ["fancy-feather-7"],
-});
+const chatClient = StreamChat.getInstance(apiKey);
+
+if (process.env.REACT_APP_CHAT_SERVER_ENDPOINT) {
+  chatClient.setBaseURL(process.env.REACT_APP_CHAT_SERVER_ENDPOINT);
+}
+
+if (typeof window !== "undefined") {
+  chatClient.connectUser({ id: userId }, userToken);
+}
 
 const App = () => (
-  <Chat client={chatClient} theme="messaging light">
-    <Channel channel={channel}>
+  <Chat client={chatClient} theme={`messaging ${theme}`}>
+    <ChannelList
+      filters={filters}
+      List={ChannelListMessenger}
+      options={options}
+      Preview={ChannelPreviewMessenger}
+      sort={sort}
+    />
+    <Channel>
       <Window>
         <ChannelHeader />
         <MessageList />
-        <MessageInput />
+        <MessageInput focus Input={MessageInputFlat} />
       </Window>
       <Thread />
     </Channel>
